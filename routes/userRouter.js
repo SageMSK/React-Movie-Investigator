695dc5e8edc18117532f441fe1c16df398f344bc
Min Soo Kim <minskim92@gmail.com>
2016-10-13 16:59:16 -0400

    fixed bug related to passport-local

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,14 +2,14 @@ const express = require('express'),
       User = require('../models/user'),
       Authentication = require('../controllers/Authentication'),
       passport = require('passport'),
-      passportService = ('./services/passport');
+      passportService = require('../services/passport');
 
 const userRouter = express.Router();
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignIn = passport.authenticate('local', { session: false });
 
 userRouter.get('/', requireAuth, (req, res, next) => {
-  res.send('HELLO');
+  res.send({ message: 'This should work' });
 });
 
 userRouter.post('/signin', requireSignIn, Authentication.signin);
