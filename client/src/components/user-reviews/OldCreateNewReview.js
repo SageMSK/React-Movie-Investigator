393863c6c0a499e4e22f468d4be0c2461617c886
Min Soo Kim <minskim92@gmail.com>
2017-10-10 11:48:38 -0400

    first commit of re-write

--- client/src/components/user-reviews/OldCreateNewReview.js
+++ client/src/components/user-reviews/OldCreateNewReview.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import TinyMCE from 'react-tinymce';
 
 import * as reviewActions from './../../actions/reviewActions';
 
@@ -30,38 +29,23 @@ const reviewField = ({ input, label, type, meta: { touched, error, warning } }) 
   <div className="form-group">
     <label>{label}</label>
     <div>
-      <TinyMCE 
-        {...input}
-        content={'Review Here'}
-        className="form-control"
-        type={type}
-        config={{
-          plugins: 'link image code',
-          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-        }}
-      />
+      <textarea type={type} className="form-control" placeholder="Write your review here." {...input} rows="10"></textarea>
       {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
     </div>
   </div>
-); // placeholder="Write your review here."
-//<textarea type={type} className="form-control" {...input} rows="10"></textarea>
+);
 
 
 let CreateNewReview = (props) => {
-  const { error, handleSubmit, pristine, reset, submitting, input } = props;
+  const { error, handleSubmit, pristine, reset, submitting } = props;
   const createNewReviewPost = (newReview) => props.createNewReview(newReview);
-  // const editorConfig = {
-  //   plugins: 'link image code',
-  //   toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-  // };
-  // <Field name="review" component={reviewField} type="textarea" label="Review" />
+
   return (
     <form onSubmit={handleSubmit(createNewReviewPost)} className="container">
       <Link to="/username/movies" className="btn">Back to List</Link>
       <Field name="title" component={renderField} type="text" label="Title" />
       <Field name="score" component={scoreField} type="number" label="Score" />
       <Field name="review" component={reviewField} type="textarea" label="Review" />
-
       <button className="btn" disabled={pristine || submitting} action="submit">Submit Review</button>
     </form>
   )
